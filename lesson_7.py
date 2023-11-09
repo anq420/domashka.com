e1983e11ddd83af87d28b6593357792fc91fbc06
Ilya <149022595+anq420@users.noreply.github.com>
2023-11-10 01:38:02 +0300

    Create lesson_7.py

---
+++ lesson_7.py
@@ -0,0 +1,24 @@
+from time import time
+from random import randint
+
+def time_check(func):
+    def calculating_time():
+        start_time = time()
+        func()
+        end_time = time()
+        return(f'Вы справились за {int(end_time-start_time)} секунд')
+    return calculating_time     
+
+@time_check
+def mathematic():
+    d1, d2 = randint(0, 1000), randint(0, 1000)
+    while True:
+        print(f'Сколько будет {d2}-{d1}?')
+        user_d = int(input('Введите ваш ответ:\n'))
+        if int(d2-d1) == user_d:
+            print('Правильно')
+            break
+        else:
+            print('Неправильно')
+
+print(mathematic())
