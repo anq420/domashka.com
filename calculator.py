1fd229aa268352e0093cc662afe3cb52aaeff7cf
Ilya <149022595+anq420@users.noreply.github.com>
2023-11-19 16:35:28 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,19 +1,24 @@
-from operations import summa, vichitanie, delenie, umnozhenie, stepen
+from operations import summa, subtraction, devision, multiply, exponentiation
+from customexceptions import WrongOption
 
 def calculator():
-    while True:
-        users_choice = input('Доступные операции:\n-Сложение\n-Вычитание\n-Умножение\n-Деление\n-Возведение в степень. Напишите название операции, чтобы её выполнить.\n')
-        if users_choice in ['Сложение', 'сложение']:
-            return summa()
-        if users_choice in ['Вычитание', 'вычитание']:
-            return vichitanie()
-        if users_choice in ['Умножение', 'умножение']:
-            return umnozhenie()
-        if users_choice in ['Деление', 'деление']:
-            return delenie()
-        if users_choice in ['Возведение в степень', 'возведение в сепень']:
-            return stepen()
-        else:
-            print('Необходимо ввести название операции!')
+    users_choice = input('Доступные операции:\n-Сложение\n-Вычитание\n-Умножение\n-Деление\n-Возведение в степень. Напишите название операции, чтобы её выполнить.\n')
+    if users_choice in ['Сложение', 'сложение']:
+        return summa()
+    if users_choice in ['Вычитание', 'вычитание']:
+        return subtraction()
+    if users_choice in ['Умножение', 'умножение']:
+        return multiply()
+    if users_choice in ['Деление', 'деление']:
+        return devision()
+    if users_choice in ['Возведение в степень', 'возведение в сепень']:
+        return exponentiation()
+    else:
+        raise WrongOption('Необходимо ввести название функции.')
 
-print(calculator())
+while True:
+    try:
+        print(calculator())
+        break
+    except WrongOption:
+        print('Необходимо правильно ввести название функции.')
