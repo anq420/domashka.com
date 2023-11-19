5f3332d04de139f3947df6a2c1d914f723d3d0e0
Ilya <149022595+anq420@users.noreply.github.com>
2023-11-19 04:25:08 +0300

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,22 +1,19 @@
 def summa():
     spisok = []
-    try:
-        a = int(input('Сколько всего чисел будем складывать?\n'))
-        if a <= 1:
-            print('Для работы функции нужно хотя бы два числа!')
-            return summa()
-    except ValueError:
-        a =  int(input('Сколько всего чисел будем складывать?\n'))
-        while a != int:
-            print('Ошибка! Ответ необходимо ввести числом! Например, 5!\nПопробуйте заново!')
-            a =  int(input('Сколько всего чисел будем складывать?\n'))
-    for i in range(a):
+    while True:
+        try:
+            a = int(input('Сколько всего чисел будем складывать?\n'))
+            if a <= 1:
+                print('Для работы функции нужно хотя бы два числа!')
+            break
+        except ValueError:
+            print('Ошибка! Ответ необходимо ввести числом! Например, 5!\nПопробуйте заново!')     
+    while len(spisok) != a:
         i = input('Введите число, которое будем складывать с другими числами:\n')
         try:
             spisok.append(float(i))
         except ValueError:
             print('Ошибка! Попробуйте заново!')
-            return summa()
     final_count = 0    
     for i in spisok:
         final_count = final_count + i
@@ -24,43 +21,41 @@ def summa():
 
 def vichitanie():
     spisok = []
-    try:
-        a = int(input('Сколько всего чисел будем использовать в вычитании?\n'))
-        if a <= 1:
-            print('Для работы функции нужно хотя бы два числа!')
-            return summa()
-    except ValueError:
-        print('Ошибка! Ответ необходимо ввести числом! Например, 5!\nПопробуйте заново!')
-        return summa()
+    while True:
+        try:
+            a = int(input('Сколько всего чисел будем использовать в вычитании?\n'))
+            if a <= 1:
+                print('Для работы функции нужно хотя бы два числа!')
+            break
+        except ValueError:
+            print('Ошибка! Ответ необходимо ввести числом! Например, 5!\nПопробуйте заново!')
     dig_1 = float(input('Введите число, из которого будем вычитать другие числа:\n'))
-    for i in range(a-1):
+    while len(spisok) != a-1:
         i = input('Введите число, которое будем отнимать от предыдущего:\n')
         try:
             spisok.append(float(i))
         except ValueError:
             print('Ошибка! Попробуйте заново!')
-            return summa()
     for i in spisok:
         dig_1 = dig_1 - i
     return dig_1
 
 def umnozhenie():
     spisok = []
-    try:
-        a = int(input('Сколько всего чисел будем умножать друг на друга?\n'))
-        if a <= 1:
-            print('Для работы функции нужно хотя бы два числа!')
-            return summa()
-    except ValueError:
-        print('Ошибка! Ответ необходимо ввести числом! Например, 5!\nПопробуйте заново!')
-        return summa()
-    for i in range(a):
+    while True:
+        try:
+            a = int(input('Сколько всего чисел будем умножать друг на друга?\n'))
+            if a <= 1:
+                print('Для работы функции нужно хотя бы два числа!')
+            break
+        except ValueError:
+            print('Ошибка! Ответ необходимо ввести числом! Например, 5!\nПопробуйте заново!')
+    while len(spisok) != a:
         i = input('Введите число, которое будем умножать на другие числа:\n')
         try:
             spisok.append(float(i))
         except ValueError:
             print('Ошибка! Попробуйте заново!')
-            return summa()
     final_count = 1    
     for i in spisok:
         final_count = final_count * i
@@ -68,22 +63,21 @@ def umnozhenie():
 
 def delenie():
     spisok = []
-    try:
-        a = int(input('Сколько всего чисел будем использовать в делении?\n'))
-        if a <= 1:
-            print('Для работы функции нужно хотя бы два числа!')
-            return summa()
-    except ValueError:
-        print('Ошибка! Ответ необходимо ввести числом! Например, 5!\nПопробуйте заново!')
-        return summa()
+    while True:
+        try:
+            a = int(input('Сколько всего чисел будем использовать в делении?\n'))
+            if a <= 1:
+                print('Для работы функции нужно хотя бы два числа!')
+            break
+        except ValueError:
+            print('Ошибка! Ответ необходимо ввести числом! Например, 5!\nПопробуйте заново!')
     dig_1 = float(input('Введите число, которое будем делить на другие числа:\n'))
-    for i in range(a-1):
+    while len(spisok) != a-1:
         i = input('Введите число, на которое будем делить предыдущее:\n')
         try:
             spisok.append(float(i))
         except ValueError:
             print('Ошибка! Попробуйте заново!')
-            return summa()
     try:
         for i in spisok:
             dig_1 = dig_1 / i
