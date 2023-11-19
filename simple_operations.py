e67ec1fc3279159d8cee8363c478caf01bda1ab2
Ilya <149022595+anq420@users.noreply.github.com>
2023-11-20 02:36:05 +0300

    Update simple_operations.py

--- simple_operations.py
+++ simple_operations.py
@@ -1,12 +1,17 @@
 from customexceptions import WrongValue
 
 def get_user_digit():
-    dig_1 = input('Введите первое число: ')
-    if not dig_1.isdigit():
-        raise WrongValue
-    dig_2 = input('Введите второе число: ')
-    if not dig_2.isdigit():
-        raise WrongValue
+    while True:
+        try:
+            dig_1 = input('Введите первое число: ')
+            if not dig_1.isdigit():
+                raise WrongValue
+            dig_2 = input('Введите второе число: ')
+            if not dig_2.isdigit():
+                raise WrongValue
+            break
+        except WrongValue:
+            print('Необходимо вводить числа!')
     return dig_1, dig_2
 
 def summa():
