963171c3ac7a21c5090d7ec89476b90565c4cf92
Ilya <149022595+anq420@users.noreply.github.com>
2023-11-20 16:27:10 +0300

    Update simple_operations.py

--- simple_operations.py
+++ simple_operations.py
@@ -33,7 +33,7 @@ def devision():
     user_digit = get_user_digit()
     dig_1, dig_2 = user_digit[0], user_digit[1]
     try:
-        return dig_1 / dig_2
+        return int(dig_1) / int(dig_2)
     except ZeroDivisionError:
         return 'На 0 делить нельзя! Ответ - 0.'
         
