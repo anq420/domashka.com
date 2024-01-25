1e6f7ce652c97f88978e1235a2b922a559169f60
Ilya <149022595+anq420@users.noreply.github.com>
2024-01-25 23:29:23 +0300

    Delete lesson 24_hw_sing-in-up directory

--- lesson 24_hw_sing-in-up/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from users.models import Users
-
-
-@admin.register(Users)
-class UsersAdmin(admin.ModelAdmin):
-    list_display = ('email', 'nickname', 'password')
-    search_fields = ('email', 'nickname')
